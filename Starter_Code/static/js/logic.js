ee8ebdc89512cce17b715adca124b48074638014
Marshal Rittenger <mrrittenger@gmail.com>
2024-03-30 16:57:54 -0700

    Code Complete (sans Legend)

---
+++ Starter_Code/static/js/logic.js
@@ -0,0 +1,63 @@
+// Bringing in the Geojson Data & creating the initial map and tile layer
+function createMap(quakeLocations) {
+  let baseMap = L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+  );
+
+  var map = L.map("map", {
+    center: [37.0902, -95.7129],
+    zoom: 4,
+    layers: [baseMap, quakeLocations],
+  });
+}
+
+function quakeColor(depth) {
+  let color;
+  if (parseInt(depth) <= 10) {
+    color = "#1a9850";
+  } else if (parseInt(depth) <= 30) {
+    color = "#91cf60";
+  } else if (parseInt(depth) <= 50) {
+    color = "#d9ef8b";
+  } else if (parseInt(depth) <= 70) {
+    color = "#fee08b";
+  } else if (parseInt(depth) <= 90) {
+    color = "#fc8d59";
+  } else {
+    color = "#d73027";
+  } return color;
+}
+
+function createMarkers(response) {
+  let quakes = response.features;
+  let quakeMarkers = [];
+
+  for (let index = 0; index < quakes.length; index++) {
+    let quake = quakes[index];
+    let magnitude = quake.properties.mag;
+
+    let quakeMarker = L.circle(
+      [quake.geometry.coordinates[1], quake.geometry.coordinates[0]],
+      {
+        radius: magnitude * 10000,
+        color: quakeColor(quake.geometry.coordinates[2]),
+        weight: .75,
+        fillColor: quakeColor(quake.geometry.coordinates[2]),
+        fillOpacity: 0.5,
+      }
+    ).bindPopup(`<h3>${quake.properties.title}</h3>
+        <h3>Date & Time: ${new Date(quake.properties.time)}</h3`);
+
+    quakeMarkers.push(quakeMarker);
+    // console.log(quakeMarkers)
+  }
+  createMap(L.layerGroup(quakeMarkers));
+}
+
+d3.json(
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+).then(createMarkers);
